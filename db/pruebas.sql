22852e1e9279d63fa21deb0377d0f6199268d519
alex-ctrl-botardo <aleslxgr34@gmail.com>
2026-06-29 17:11:59 +0200

    Actualizar dump base de datos

---
+++ db/pruebas.sql
@@ -0,0 +1,136 @@
+-- MariaDB dump 10.19  Distrib 10.4.28-MariaDB, for Linux (x86_64)
+--
+-- Host: localhost    Database: pruebas
+-- ------------------------------------------------------
+-- Server version	10.4.28-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `Marca`
+--
+
+DROP TABLE IF EXISTS `Marca`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Marca` (
+  `id_marca` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(100) NOT NULL,
+  `pais` varchar(100) NOT NULL,
+  PRIMARY KEY (`id_marca`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Marca`
+--
+
+LOCK TABLES `Marca` WRITE;
+/*!40000 ALTER TABLE `Marca` DISABLE KEYS */;
+INSERT INTO `Marca` VALUES (1,'Toyota','Japón'),(2,'BMW','Alemania'),(3,'Audi','Alemania'),(5,'Seat','España'),(6,'Fiat','Italia'),(7,'Honda','Japón'),(8,'Ferrari','Italia'),(9,'Masserati','Italia'),(10,'Mercedes','Alemania');
+/*!40000 ALTER TABLE `Marca` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Modelo`
+--
+
+DROP TABLE IF EXISTS `Modelo`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Modelo` (
+  `id_modelo` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(100) NOT NULL,
+  `id_motor` int(11) NOT NULL,
+  PRIMARY KEY (`id_modelo`) USING BTREE,
+  KEY `fk_modelo_motor` (`id_motor`) USING BTREE,
+  CONSTRAINT `fk_modelo_motor` FOREIGN KEY (`id_motor`) REFERENCES `Motor` (`Id_motor`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Modelo`
+--
+
+LOCK TABLES `Modelo` WRITE;
+/*!40000 ALTER TABLE `Modelo` DISABLE KEYS */;
+INSERT INTO `Modelo` VALUES (1,'Corola',1),(2,'Yaris',1),(3,'M3',2),(4,'Ibiza',5),(5,'GR Yaris',4),(7,'Supra MK IV',6);
+/*!40000 ALTER TABLE `Modelo` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Motor`
+--
+
+DROP TABLE IF EXISTS `Motor`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Motor` (
+  `id_motor` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(100) NOT NULL,
+  `potencia` varchar(100) NOT NULL,
+  `par` varchar(100) NOT NULL,
+  `cilindrada` decimal(10,4) NOT NULL,
+  `num_pistones` int(11) NOT NULL,
+  `id_marca` int(11) NOT NULL,
+  PRIMARY KEY (`id_motor`) USING BTREE,
+  KEY `fk_motor_marca` (`id_marca`),
+  CONSTRAINT `fk_motor_marca` FOREIGN KEY (`id_marca`) REFERENCES `Marca` (`Id_marca`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Motor`
+--
+
+LOCK TABLES `Motor` WRITE;
+/*!40000 ALTER TABLE `Motor` DISABLE KEYS */;
+INSERT INTO `Motor` VALUES (1,'2.0 TDI','150','340',1968.0000,4,1),(2,'2.3 TDI','180','380',2268.0000,4,2),(4,'1.8 Hybrid 200H','200','400',1760.0000,4,1),(5,'1.8 TDI','130','370',1720.0000,4,5),(6,'2jz','200','600',1200.0000,4,1);
+/*!40000 ALTER TABLE `Motor` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Usuario`
+--
+
+DROP TABLE IF EXISTS `Usuario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Usuario` (
+  `id_usuario` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `rol` enum('admin','usuario') DEFAULT 'usuario',
+  PRIMARY KEY (`id_usuario`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Usuario`
+--
+
+LOCK TABLES `Usuario` WRITE;
+/*!40000 ALTER TABLE `Usuario` DISABLE KEYS */;
+INSERT INTO `Usuario` VALUES (1,'admin','$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi','admin'),(2,'alejandro','$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi','usuario');
+/*!40000 ALTER TABLE `Usuario` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2026-06-29 17:11:59
